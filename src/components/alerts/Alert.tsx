25ce3091c06ee5164def190c94d92912e5b7cfd9
Scott Byrne <scott.byrne@forestry.io>
2022-02-03 18:37:46 -0400

    alert

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const Alert = ({ isOpen, message, variant = 'warning' }: Props) => {
   const classes = classNames(
-    'fixed bottom-0 z-[1000] left-1/2 transform -translate-x-1/2 max-w-sm w-full shadow-lg rounded-t-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden',
+    'fixed bottom-0 z-[1000] left-1/2 transform -translate-x-1/2 max-w-sm shadow-lg rounded-t-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden',
     {
       'bg-rose-800': variant === 'warning',
       'bg-green-700': variant === 'success',
@@ -22,15 +22,15 @@ export const Alert = ({ isOpen, message, variant = 'warning' }: Props) => {
       show={isOpen}
       as={Fragment}
       enter="ease-out duration-300 transition"
-      enterFrom="opacity-0 -translate-y-1/2"
+      enterFrom="opacity-0 translate-y-1/2"
       enterTo="opacity-100"
       leave="transition ease-in duration-100"
       leaveFrom="opacity-100"
-      leaveTo="opacity-0 -translate-y-1/2"
+      leaveTo="opacity-0 translate-y-1/2"
     >
       <div className={classes}>
-        <div className="p-4">
-          <p className="text-lg md:text-xl text-center font-bold text-zinc-200">
+        <div className="py-2 px-6 sm:py-4 lg:px-12">
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl text-center font-bold text-zinc-200">
             {message}
           </p>
         </div>
